d47da13eea5ee15bfe6a3dd051c5820c80bb324d
Hao <ghmail2@gmail.com>
2015-04-21 15:24:56 +0200

    remove sub address

--- src/main/java/course/FirstController.java
+++ src/main/java/course/FirstController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping("/")
 	
-	public String greeting(@RequestParam(value="name",required=false,defaultValue="World")String name,Model model){
+	public String index(@RequestParam(value="name",required=false,defaultValue="World")String name,Model model){
 		model.addAttribute("name", name);
-		return "greeting";
+		return "index";
 		
 		
 		
